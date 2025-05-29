7aa11c9ed3bd8e875b2c67ee6c4ad0dcd8e02a9a
sabalicious <vladchermenin4@gmail.com>
2025-05-29 03:45:08 +0300

    add main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// ===== main.cpp =====
+#include "MainWindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    MainWindow w;
+    w.show();
+
+    return app.exec();
+}
